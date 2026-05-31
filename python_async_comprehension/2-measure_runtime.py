cefa45d08de86f4c0fd9262f2ce24fa014427377
TurkanBaghirzade✮⋆˙ <137319134+Prenses123@users.noreply.github.com>
2026-06-01 00:14:56 +0400

    Update 2-measure_runtime.py

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,12 +3,17 @@
 
 import asyncio
 import time
+
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """Measure the total runtime of async_comprehension."""
     start_time = time.time()
-    await asyncio.gather(async_comprehension() for _ in range(4))
+    
+    # List comprehension yaratdıq və '*' ilə elementləri ayıraraq gather-a verdik
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    
     end_time = time.time()
     return end_time - start_time
+    
